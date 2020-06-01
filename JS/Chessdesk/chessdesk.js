ace000eae85bc31ccd93d3d83bcea1e23875e5ea
Yaroslav Gelem <gelem.cyfra@gmail.com>
2020-06-01 17:11:37 +0300

    v1.1

--- JS/Chessdesk/chessdesk.js
+++ JS/Chessdesk/chessdesk.js
@@ -1,19 +1,16 @@
 var container = document.getElementById("container");
-for (var i = 1; i <= 8; i++){
-    var newRow = document.createElement("div");
-    newRow.setAttribute("class", "row");
-    for (var j = 1; j <= 8; j++){
-        var newCell = document.createElement("div");
-        if ((i % 2 == 0) && (j % 2 !== 0)) {
-            newCell.setAttribute("class", "cell cell_black");
-        } else if ((i % 2 !== 0) && (j % 2 == 0)) {
-            newCell.setAttribute("class", "cell cell_black");
-        } else {
-            newCell.setAttribute("class", "cell cell_white");
-        }
-        newRow.appendChild(newCell);
-    }
-    container.appendChild(newRow);
-        
+for (var i = 1; i <= 8; i++) {
+  var newRow = document.createElement("div");
+  newRow.setAttribute("class", "row");
+  for (var j = 1; j <= 8; j++) {
+    var newCell = document.createElement("div");
+    var _classblwh = "";
+    _classblwh =
+      (i % 2 == 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 == 0)
+        ? "cell cell_black"
+        : "cell cell_white";
+    newCell.setAttribute("class", _classblwh);
+    newRow.appendChild(newCell);
   }
-
+  container.appendChild(newRow);
+}
