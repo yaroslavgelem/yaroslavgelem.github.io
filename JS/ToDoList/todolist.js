35d8fcfc5ee247c047bb6cd3f9cfb0a02cdf083a
Yaroslav Gelem <gelem.cyfra@gmail.com>
2020-06-04 09:50:23 +0300

    v1.1

--- JS/ToDoList/todolist.js
+++ JS/ToDoList/todolist.js
@@ -1,24 +1,71 @@
 var body = document.getElementsByTagName("body")[0];
+var header = document.createElement ("header");
 var maintitle = document.createElement("h1");
-maintitle.innerText = "To Do List";
 var textarea = document.getElementById("textarea");
-body.insertBefore(maintitle, textarea);
-console.log(textarea.value);
+var button = document.getElementById("add_btn");
+var container = document.getElementById("content");
+body.insertBefore(header,container);
+maintitle.innerText = "To Do List";
+header.appendChild(maintitle);
+header.appendChild(textarea);
+header.appendChild(button);
+header.insertBefore(maintitle, textarea);
+
+
+
 
 //Create a new element to list
 function addListItem() {
-  var container = document.getElementById("content");
-  var divList = document.createElement("div");
   var inputValue = textarea.value;
-  container.appendChild(divList);
   if (inputValue === "") {
     alert("Потрібно щось написати!");
   } else {
-    divList.className = "list";
+    var divItem = document.createElement ("div");
+    var newDiv = document.createElement("div");
+    var imageCircle = document.createElement("img");
+    var newP = document.createElement("p");
+    var buttonPlus = document.createElement("button");
+    var imagePlus = document.createElement ("img");
+    var buttonClose = document.createElement("button");
+    var imageClose = document.createElement ("img");
+    container.appendChild(divItem);
+    divItem.appendChild(newDiv);
+    divItem.appendChild(buttonPlus);
+    buttonPlus.appendChild(imagePlus);
+    divItem.appendChild(buttonClose);
+    buttonClose.appendChild(imageClose);
+    newDiv.appendChild(imageCircle);
+    newDiv.appendChild(newP);
+    divItem.className = "item";
+    newDiv.className = "list not_checked";
+    imageCircle.className = "image";
+    imageCircle.src = "images/notcheck.png";
+    newP.innerText = inputValue;
+    buttonPlus.className = "btn item_btn plus_btn";
+    imagePlus.src = "images/plus_buttom.png";
+    buttonClose.className = "btn item_btn close_btn";
+    imageClose.src = "image/close_button.png";
   }
-  divList.innerText = inputValue ;
+  document.getElementById("textarea").value = "";
 }
 
 // Programing button
-var button = document.getElementById("add_btn");
 button.addEventListener("click", addListItem);
+
+// Create checked list item
+function mouseClick(e) {
+  e.target.className == "list not_checked"
+    ? (e.target.className = "list checked")
+    : e.target.className == "list checked"
+    ? (e.target.className = "list not_checked")
+    : e.target.className == "list not_checked";
+}
+var divList = document.getElementById("content");
+console.log(divList)
+// if (item)
+// var divList = document.getElementsByClassName("list not_checked");
+// divList.addEventListener("click", function mouseClick(e){});
+// var imageList = document.getElementsByClassName("image")[0];
+// imageList.addEventListener("click", function mouseClick(e){});
+
+// Add delete button
